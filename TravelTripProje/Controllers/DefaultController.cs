f4f4b03fca17c4796436d9704548bd4990fd3f80
osman28tr <osmantonbul2001@gmail.com>
2022-06-23 13:00:52 +0300

    Hakkımızda ve Rehber Sayfaları

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -13,5 +13,10 @@ namespace TravelTripProje.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
     }
 }
