253327fa5c20312f907a27aae41daa71b5e31c3b
osman28tr <osmantonbul2001@gmail.com>
2022-07-04 18:09:03 +0300

    Ana sayfanın tamamlanmasi,Admin Paneli,Blog Listesi(Admin) islemleri yapildi.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Blogs.ToList();
+            return View(degerler);
+        }
+    }
+}
