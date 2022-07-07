6e3c4791eceba068fffd2e26f99dec06abedd712
osman28tr <osmantonbul2001@gmail.com>
2022-07-07 23:33:13 +0300

    İletişim arayüzü işlemleri gerçekleştirildi.

---
+++ TravelTripProje/Controllers/IletisimController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        Context context = new Context();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(İletisim iletisim)
+        {
+            context.İletisims.Add(iletisim);
+            context.SaveChanges();
+            return RedirectToAction("Index", new { controller = "Default" });
+        }
+    }
+}
