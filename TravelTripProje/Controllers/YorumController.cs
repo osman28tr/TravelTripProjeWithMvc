b051f3d51d6b3ea0f7f1585afa3eba32aa4a1cae
osman28tr <osmantonbul2001@gmail.com>
2022-07-07 16:48:19 +0300

    Admin tarafında yorum listeleme,silme,güncelleme işlemleri gerçekleştirildi.

---
+++ TravelTripProje/Controllers/YorumController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class YorumController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var yorumlar = context.Yorums.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var yorum = context.Yorums.Find(id);
+            context.Yorums.Remove(yorum);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var yorum = context.Yorums.Find(id);
+            return View("YorumGetir", yorum);
+        }
+        public ActionResult YorumGuncelle(Yorum yorum)
+        {
+            var yeniYorum = context.Yorums.Find(yorum.ID);
+            yeniYorum.KullaniciAdi = yorum.KullaniciAdi;
+            yeniYorum.YorumYazi = yorum.YorumYazi;
+            yeniYorum.Mail = yorum.Mail;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
