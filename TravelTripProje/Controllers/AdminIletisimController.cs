d1786008faf1555553e3de1b1948cbfd45e63920
osman28tr <osmantonbul2001@gmail.com>
2022-07-07 23:42:06 +0300

    Admin tarafında iletişim paneli işlemleri gerçekleştirildi

---
+++ TravelTripProje/Controllers/AdminIletisimController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminIletisimController : Controller
+    {
+        // GET: AdminIletisim
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var mesajlar = context.İletisims.ToList();
+            return View(mesajlar);
+        }
+    }
+}
