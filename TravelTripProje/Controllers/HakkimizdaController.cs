34d35bfe42319ca268f6070e70a73ea6160195e6
osman28tr <osmantonbul2001@gmail.com>
2022-07-08 00:02:32 +0300

    Hakkimizda arayüzü ile ilgili işlemler gerçekleştirildi.

---
+++ TravelTripProje/Controllers/HakkimizdaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        // GET: Hakkimizda
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
